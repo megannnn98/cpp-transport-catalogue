28d81611066c83c265069e12817a52fea0fdc262
Mikhail Karatanov <m.karatanov@integrasources.com>
2022-09-22 08:48:30 +0700

    feat: request handler moved

--- request_handler.cpp
+++ request_handler.cpp
@@ -1,9 +1,44 @@
 #include "request_handler.h"
 
-/*
- * Здесь можно было бы разместить код обработчика запросов к базе, содержащего логику, которую не
- * хотелось бы помещать ни в transport_catalogue, ни в json reader.
- *
- * Если вы затрудняетесь выбрать, что можно было бы поместить в этот файл,
- * можете оставить его пустым.
- */
+namespace tc
+{
+RequestHandler::RequestHandler(const TransportCatalogue& db, const renderer::RenderSettings& settings)
+    : db_{db}, renderer_{settings}
+{
+}
+
+[[nodiscard]] const std::unordered_set<const domain::Bus*>& RequestHandler::GetBusesFromStop(std::string_view name) const
+{
+    return db_.GetBusesFromStop(name);
+}
+
+[[nodiscard]] const domain::Stop& RequestHandler::GetStop(std::string_view name) const
+{
+    return db_.GetStop(name);
+}
+
+[[nodiscard]] const domain::Bus& RequestHandler::GetBus(std::string_view name) const
+{
+    return db_.GetBus(name);
+}
+
+[[nodiscard]] const std::vector<const domain::Stop*>& RequestHandler::GetStopsFromBus(std::string_view name) const
+{
+    return db_.GetStopsFromBus(name);
+}
+
+[[nodiscard]] const geo::Coordinates& RequestHandler::GetStopCoords(std::string_view name) const
+{
+    return db_.GetStopCoords(name);
+}
+
+std::uint32_t RequestHandler::GetDistanceBetween(std::string_view nameA, std::string_view nameB) const
+{
+    return db_.GetDistanceBetween(nameA, nameB);
+}
+
+void RequestHandler::MakeDoc(svg::Document& doc) const
+{
+    renderer_.MakeDoc(doc, db_.GetBuses());
+}
+} // namespace
--- request_handler.h
+++ request_handler.h
@@ -19,45 +19,14 @@ class RequestHandler
     const TransportCatalogue& db_;
     const renderer::MapRenderer renderer_;
 public:
-    explicit RequestHandler(const TransportCatalogue& db, const renderer::RenderSettings& settings)
-        : db_{db}, renderer_{settings}
-    {
-    }
-
-    [[nodiscard]] const auto& GetBusesFromStop(std::string_view name) const
-    {
-        return db_.GetBusesFromStop(name);
-    }
-
-    [[nodiscard]] const domain::Stop& GetStop(std::string_view name) const
-    {
-        return db_.GetStop(name);
-    }
-
-    [[nodiscard]] const domain::Bus& GetBus(std::string_view name) const
-    {
-        return db_.GetBus(name);
-    }
-
-    [[nodiscard]] const std::vector<const domain::Stop*>& GetStopsFromBus(std::string_view name) const
-    {
-        return db_.GetStopsFromBus(name);
-    }
-
-    [[nodiscard]] const geo::Coordinates& GetStopCoords(std::string_view name) const
-    {
-        return db_.GetStopCoords(name);
-    }
-
-    std::uint32_t GetDistanceBetween(std::string_view nameA, std::string_view nameB) const
-    {
-        return db_.GetDistanceBetween(nameA, nameB);
-    }
-
-    void MakeDoc(svg::Document& doc) const
-    {
-        renderer_.MakeDoc(doc, db_.GetBuses());
-    }
+    explicit RequestHandler(const TransportCatalogue& db, const renderer::RenderSettings& settings);
+    [[nodiscard]] const std::unordered_set<const domain::Bus*>& GetBusesFromStop(std::string_view name) const;
+    [[nodiscard]] const domain::Stop& GetStop(std::string_view name) const;
+    [[nodiscard]] const domain::Bus& GetBus(std::string_view name) const;
+    [[nodiscard]] const std::vector<const domain::Stop*>& GetStopsFromBus(std::string_view name) const;
+    [[nodiscard]] const geo::Coordinates& GetStopCoords(std::string_view name) const;
+    std::uint32_t GetDistanceBetween(std::string_view nameA, std::string_view nameB) const;
+    void MakeDoc(svg::Document& doc) const;
 
 };
 
